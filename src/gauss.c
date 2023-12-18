a5067df3c47d7f4b19ee35aafd800b48e14f53cb
Karol Krukowski <01178546@pw.edu.pl>
2023-12-18 21:15:38 +0100

    Napisanie algorytmu eliminacji Gaussa

--- src/gauss.c
+++ src/gauss.c
@@ -1,14 +1,26 @@
 #include "gauss.h"
 
 /**
- * Zwraca 0 - elimnacja zakonczona sukcesem
- * Zwraca 1 - macierz osobliwa - dzielenie przez 0
- */
+Zwraca 0 - elimnacja zakonczona sukcesem
+Zwraca 1 - macierz osobliwa - dzielenie przez 0
+*/
+
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+    
+	int i, j, k;
 
-		return 0;
+    for (i = 0; i < mat->r - 1; i++){
+        if (mat->data[i][i] == 0.0){
+            return 1;
+		}
+		for (k = i + 1; k < mat->r; k++){
+            double factor = mat->data[k][i] / mat->data[i][i];
+            for (j = i; j < mat->c; j++){
+               mat->data[k][j] -= factor * mat->data[i][j];
+            }
+            b->data[k][0] -= factor * b->data[i][0];
+        }
+    }
+
+    return 0;
 }
-
