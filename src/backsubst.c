5600c6fe3633c730a9f7fdc62f61418dabf6c919
Vertexcell <vertexcell@gmail.com>
2023-12-18 21:33:27 +0100

    Dodanie algorytmu podstawienia wstecznego

--- src/backsubst.c
+++ src/backsubst.c
@@ -5,18 +5,26 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+	if (mat->r != mat->c || mat->r != b->r || x->r != b->r || x->c != 1) {
+		printf("Nieprawidlowy rozmiar macierzy\n");
+        return 2;
+	}
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+	int n = mat->r;
+	for (int w = n - 1; w >= 0; w--) {
+        if (mat->data[w][w] == 0) {
+		printf("Element na diagonali jest rowny 0");
+            return 1;
+        }
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+        double s = 0;
+        for (int k = w + 1; k < n; k++) {
+            s += mat->data[w][k] * x->data[k][0];
+        }
+        x->data[w][0] = (b->data[w][0] - s) / mat->data[w][w];
+	}
 
-				return 0;
+	return 0;
 }
 
 
